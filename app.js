6a50c13263991b053fc4a35f6ef85974bed7ba5e
H Dyson <Hldyson@hotmail.com>
2024-04-24 16:57:37 +0000

    update to index.html and css.

--- app.js
+++ app.js
@@ -1,62 +1,70 @@
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let gradient = ctx.createRadialGradient(canvas.width/2, canvas.height/2, 100, canvas.width/2, canvas.height/2, 200);
-//gradient.addColorStop(0, 'rgb(76, 37, 76)');
-//gradient.addColorStop(0.2, 'yellow');
-//gradient.addColorStop(0.4, 'green');
-//gradient.addColorStop(0.6, 'cyan');
-//gradient.addColorStop(0.8, 'blue');
-gradient.addColorStop(1, 'limegreen');
-
+let gradient = ctx.createRadialGradient(
+  canvas.width / 2,
+  canvas.height / 2,
+  100,
+  canvas.width / 2,
+  canvas.height / 2,
+  200
+);
+// gradient.addColorStop(0, "rgb(76, 37, 76)");
+// gradient.addColorStop(0.2, "yellow");
+// gradient.addColorStop(0.4, "green");
+// gradient.addColorStop(0.6, "cyan");
+// gradient.addColorStop(0.8, "blue");
+// gradient.addColorStop(1, "limegreen");
+gradient.addColorStop(1, "rgb(197, 197, 197)");
 
 class Symbol {
-    constructor(x, y, fontSize, canvasHeight){
-        this.characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"£$%^&*()';
-        this.x = x;
-        this.y = y; 
-        this.fontSize = fontSize;
-        this.text = '';
-        this.canvasHeight = canvasHeight;
-
+  constructor(x, y, fontSize, canvasHeight) {
+    this.characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"£$%^&*()';
+    this.x = x;
+    this.y = y;
+    this.fontSize = fontSize;
+    this.text = "";
+    this.canvasHeight = canvasHeight;
+  }
+  draw(context) {
+    this.text = this.characters.charAt(
+      Math.floor(Math.random() * this.characters.length)
+    );
+    //        context.fillStyle ='green';
+    context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+    if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
+      this.y = 0;
+    } else {
+      this.y += 1;
     }
-    draw(context){
-        this.text = this.characters.charAt(Math.floor(Math.random() * this.characters.length));
-//        context.fillStyle ='green';
-        context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
-        if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98){
-            this.y = 0;
-        } else {
-            this.y += 1;
-        }
-    }
+  }
 }
 
 class Effect {
-    constructor(canvasWidth, canvasHeight) {
-        this.canvasWidth = canvasWidth;
-        this.canvasHeight = canvasHeight;
-        this.fontSize = 15;
-        this.columns = this.canvasWidth / this.fontSize;
-        this.symbols = [];
-        this.initialize();
-    }
+  constructor(canvasWidth, canvasHeight) {
+    this.canvasWidth = canvasWidth;
+    this.canvasHeight = canvasHeight;
+    this.fontSize = 15;
+    this.columns = this.canvasWidth / this.fontSize;
+    this.symbols = [];
+    this.initialize();
+  }
 
-    initialize() {
-        for (let i = 0; i < this.columns; i++) {
-            this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
-        }
+  initialize() {
+    for (let i = 0; i < this.columns; i++) {
+      this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
     }
-    resize(width, height){
-        this.canvasWidth = width;
-        this.canvasHeight = height;
-        this.columns = this.canvasWidth / this.fontSize;
-        this.symbols = [];
-        this.initialize();
-
-    }
+  }
+  resize(width, height) {
+    this.canvasWidth = width;
+    this.canvasHeight = height;
+    this.columns = this.canvasWidth / this.fontSize;
+    this.symbols = [];
+    this.initialize();
+  }
 }
 
 const effect = new Effect(canvas.width, canvas.height);
@@ -65,29 +73,27 @@ const fps = 40;
 const nextFrame = 1000 / fps;
 let timer = 0;
 
-function animate(timestamp){
-    const deltaTime = timestamp - lastTime;
-    lastTime = timestamp;
-    if (timer > nextFrame){
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
-    ctx.textAlign = 'center';
+function animate(timestamp) {
+  const deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+  if (timer > nextFrame) {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    ctx.textAlign = "center";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = gradient; //'#0aff0a';
-    ctx.font = effect.fontSize + 'px monospace';
-    effect.symbols.forEach(symbol => symbol.draw(ctx));
+    ctx.font = effect.fontSize + "px monospace";
+    effect.symbols.forEach((symbol) => symbol.draw(ctx));
     timer = 0;
-    }else {
-        timer += deltaTime;
-    }    
-    requestAnimationFrame(animate);
-
+  } else {
+    timer += deltaTime;
+  }
+  requestAnimationFrame(animate);
 }
 
 animate(0);
 
-window.addEventListener('resize', function(){
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    effect.resize(canvas.width, canvas.height);
-}
-)
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  effect.resize(canvas.width, canvas.height);
+});
